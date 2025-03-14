22752837abdec98ba77e13307d385aab8063051b
luojiahai <ljiahai@hotmail.com>
2025-03-14 12:25:25 +1100

    Update

---
+++ example/README.md
@@ -0,0 +1,7 @@
+# Example
+
+Run example:
+
+```shell
+poetry run example
+```
--- example/__init__.py
+++ example/__init__.py
@@ -1,22 +1,21 @@
-import repli
-import repli.callback
+from repli import Command, Interpreter, Page
+from repli.callback import NativeFunction, Subprocess
 
 
 def main():
-    native_function = repli.callback.NativeFunction(function=lambda *args: print('command 1'))
-    subprocess = repli.callback.Subprocess(arguments=lambda *args: 'echo command 2')
+    native_function = NativeFunction(function=lambda *args: print('command 1'))
+    subprocess = Subprocess(arguments=lambda *args: 'echo command 2')
     commands = [
-        repli.Command(name='1', description='command 1', callback=native_function),
-        repli.Command(name='2', description='command 2', callback=subprocess),
+        Command(name='1', description='command 1', callback=native_function),
+        Command(name='2', description='command 2', callback=subprocess),
     ]
     elements = [
-        repli.Page(
+        Page(
             name='1',
             description='page 1',
             elements=commands,
         ),
     ]
-    page = repli.Page(name='0', description='home', elements=elements)
-
-    interpreter = repli.Interpreter(page=page)
+    page = Page(name='0', description='home', elements=elements)
+    interpreter = Interpreter(page=page)
     interpreter.loop()
